929c36e86761b96c4a57f57624c88f04a5882656
tool7 <marko.roje@gmail.com>
2019-11-30 18:13:18 +0100

    Implemented A* search

---
+++ src/ts/algorithms/pathfinding/a-star-search/a-star-search.ts
@@ -0,0 +1,56 @@
+import PriorityQueue from "../priority-queue";
+import HashMap from "../hash-map";
+import PathfindingResult from "../pathfinding-result";
+import { WeightedGraph, Node } from "../graph";
+
+// Heuristic function that tells us how close we are to the goal
+function heuristic(nodeA: Node, nodeB: Node): number {
+  return Math.abs(nodeA.x - nodeB.x) + Math.abs(nodeA.y - nodeB.y);
+}
+
+function aStarSearch(graph: WeightedGraph, start: Node, goal: Node): PathfindingResult {
+  let frontier = new PriorityQueue<Node>();
+  let cameFrom = new HashMap();
+  let costSoFar = new HashMap();
+  let visited: Node[] = [];
+  let path: Node[] = [];
+
+  frontier.enqueue(start, 0);
+  cameFrom.set(start, null);
+  costSoFar.set(start, 0);
+
+  while (!frontier.isEmpty()) {
+    const current = frontier.dequeue();
+
+    // Adding to visited array only because of visualization
+    visited.push(current);
+
+    if (graph.areEqual(current, goal)) { break; }
+
+    graph.neighbors(current).forEach((next: any) => {
+      const newCost = costSoFar.get(current) + graph.cost(current, next);
+
+      // If "next" was not yet visited or new cost is lower than
+      // previously calculated cost to "next"
+      if (!costSoFar.contains(next) || newCost < costSoFar.get(next)) {
+        costSoFar.set(next, newCost);
+        cameFrom.set(next, current);
+
+        const priority = newCost + heuristic(next, goal);
+        frontier.enqueue(next, priority);
+      }
+    });
+  }
+
+  // Reconstructing the path
+  let current = goal;
+  while (!graph.areEqual(current, start)) {
+    path.push(current);
+    current = cameFrom.get(current);
+  }
+  path.reverse();
+
+  return { path, visited };
+}
+
+export default aStarSearch;
